111eb20812b22bb63c20ce3a86826049763c0317
YOJI4910 <fmn.outlook@outlook.com>
2019-08-23 23:35:07 +0900

    Add register form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,27 @@
 class UsersController < ApplicationController
   def index
-    @users = User.new
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    user.save!
+    redirect_to root_url, notice:'ユーザ登録を行いました。'
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :height, :password_digest)
   end
 end
