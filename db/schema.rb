c7cbdba8737a2fb95bbc58aa6ac7f2d1b762c3c7
YOJI4910 <fmn.outlook@outlook.com>
2019-09-02 02:26:14 +0900

    Change register page design

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_100443) do
+ActiveRecord::Schema.define(version: 2019_09_01_091427) do
 
   create_table "records", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.float "weight"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_08_24_100443) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
