1174f1959c02df04767ef43b748f53b3b912421a
YOJI4910 <fmn.outlook@outlook.com>
2019-12-16 04:12:41 +0900

    Migrate some columns for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  has_secure_password
-  
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+           
   before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
