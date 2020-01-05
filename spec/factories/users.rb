2ea371cd567da3566c344bc1d45789849a89d663
YOJI4910 <fmn.outlook@outlook.com>
2020-01-05 16:36:47 +0900

    [add]factory_botのuserを作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,35 @@
 FactoryBot.define do
   factory :user do
     name { 'テストユーザー' }
-    email { 'test1@example.com' }
+    sequence(:email) { |n| "test-user-#{n}@example.com" }
     height { 174.2 }
     password { 'password' }
+
+    trait :name_invalid do
+      name {''}
+    end
+
+    trait :email_invalid do
+      email {'test-user'} # @以降がない
+    end
+
+    trait :password_invalid do
+      password {'pass'} # 6文字以上ない
+    end
+
+    trait :guest do
+      name {'guest-user'}
+      email {'guest@example.com'}
+    end
+
+    trait :with_records do
+      transient do
+        record_count 5
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:record, evaluator.record_count, user: user)
+      end
+    end
   end
 end
